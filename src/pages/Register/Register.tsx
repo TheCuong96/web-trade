6ace8124c87149b8c3e440a5884946fffd91f8a8
thecuong1396 <cuongphan@coinmap.tech>
2023-02-28 05:30:08 +0700

    xác thực input với schema và Yup trong react-hook-form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,13 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import { getRules } from 'src/utils/rules'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { schema, Schema } from 'src/utils/rules'
 import Input from 'src/components/Input'
 
-interface FormData {
-  email: string
-  password: string
-  confirm_password: string
-}
+type FormData = Schema
 
 export default function Register() {
   const {
@@ -16,8 +13,9 @@ export default function Register() {
     watch,
     getValues,
     formState: { errors }
-  } = useForm<FormData>()
-  const rules = getRules(getValues)
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
   const onSubmit = handleSubmit(
     (data) => {
       console.log(data)
@@ -29,8 +27,8 @@ export default function Register() {
     }
   )
 
-  // const email = watch('password')
-  // console.log(email)
+  const value = watch()
+  console.log(value)
 
   return (
     <div className='bg-skyblue'>
@@ -50,7 +48,6 @@ export default function Register() {
                 className='mt-8'
                 errorMessage={errors.email?.message}
                 placeholder='Email'
-                rules={rules.email}
               />
               <Input
                 name='password'
@@ -59,7 +56,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.password?.message}
                 placeholder='Password'
-                rules={rules.password}
                 autoComplete='on'
               />
 
@@ -70,7 +66,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.confirm_password?.message}
                 placeholder='Confirm Password'
-                rules={rules.confirm_password}
                 autoComplete='on'
               />
               <div className='mt-2'>
