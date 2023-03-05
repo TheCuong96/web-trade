a6a006b2b3788896026acb493726d82640f58b20
thecuong1396 <cuongphan@coinmap.tech>
2023-03-05 16:30:37 +0700

    Code Slider cho ảnh sản phẩm

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import DOMPurify from 'dompurify'
+import { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
 import InputNumber from 'src/components/InputNumber'
 import ProductRating from 'src/components/ProductRating'
+import { Product } from 'src/types/product.type'
 import {
   formatCurrency,
   formatNumberToSocialStyle,
@@ -16,23 +18,57 @@ export default function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImage, setActiveImage] = useState('')
   const product = productDetailData?.data.data
+
+  const currentImages = useMemo(
+    () => (product ? product.images.slice(...currentIndexImages) : []),
+    [product, currentIndexImages]
+  )
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImage(product.images[0])
+    }
+  }, [product])
+
+  const next = () => {
+    console.log(currentIndexImages[1])
+    if (currentIndexImages[1] < (product as Product).images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+
+  const prev = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
+  const chooseActive = (img: string) => {
+    setActiveImage(img)
+  }
+
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
-                  src={product.image}
+                  src={activeImage}
                   alt={product.name}
                   className='absolute top-0 left-0 h-full w-full bg-white object-cover'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={prev}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -48,22 +84,29 @@ export default function ProductDetail() {
                     />
                   </svg>
                 </button>
-                {product.images.slice(0, 5).map((img, index) => {
-                  const isActive = index === 0
+                {currentImages.map((img) => {
+                  const isActive = img === activeImage
                   return (
-                    <div className='relative w-full pt-[100%]' key={img}>
+                    <div
+                      className='relative w-full pt-[100%]'
+                      key={img}
+                      onMouseEnter={() => chooseActive(img)}
+                    >
                       <img
-                        src={product.image}
+                        src={img}
                         alt={product.name}
                         className='absolute top-0 left-0 h-full w-full cursor-pointer bg-white object-cover'
                       />
                       {isActive && (
-                        <div className='border-orange absolute inset-0 border-2' />
+                        <div className='absolute inset-0 border-2 border-skyblue' />
                       )}
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={next}
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -85,12 +128,12 @@ export default function ProductDetail() {
               <h1 className='text-xl font-medium uppercase'>{product.name}</h1>
               <div className='mt-8 flex items-center'>
                 <div className='flex items-center'>
-                  <span className='border-b-orange text-orange mr-1 border-b'>
+                  <span className='mr-1 border-b border-b-skyblue text-skyblue'>
                     {product.rating}
                   </span>
                   <ProductRating
                     rating={product.rating}
-                    activeClassname='fill-orange text-orange h-4 w-4'
+                    activeClassname='fill-skyblue text-skyblue h-4 w-4'
                     nonActiveClassname='fill-gray-300 text-gray-300 h-4 w-4'
                   />
                 </div>
@@ -104,10 +147,10 @@ export default function ProductDetail() {
                 <div className='text-gray-500 line-through'>
                   ₫{formatCurrency(product.price_before_discount)}
                 </div>
-                <div className='text-orange ml-3 text-3xl font-medium'>
+                <div className='ml-3 text-3xl font-medium text-skyblue'>
                   ₫{formatCurrency(product.price)}
                 </div>
-                <div className='bg-orange ml-4 rounded-sm px-1 py-[2px] text-xs font-semibold uppercase text-white'>
+                <div className='ml-4 rounded-sm bg-skyblue px-1 py-[2px] text-xs font-semibold uppercase text-white'>
                   {rateSale(product.price_before_discount, product.price)} giảm
                 </div>
               </div>
@@ -158,13 +201,13 @@ export default function ProductDetail() {
                 </div>
               </div>
               <div className='mt-8 flex items-center'>
-                <button className='border-orange bg-orange/10 text-orange hover:bg-orange/5 flex h-12 items-center justify-center rounded-sm border px-5 capitalize shadow-sm'>
+                <button className='flex h-12 items-center justify-center rounded-sm border border-skyblue bg-skyblue/10 px-5 capitalize text-skyblue shadow-sm hover:bg-skyblue/5'>
                   <svg
                     enableBackground='new 0 0 15 15'
                     viewBox='0 0 15 15'
                     x={0}
                     y={0}
-                    className='stroke-orange text-orange mr-[10px] h-5 w-5 fill-current'
+                    className='mr-[10px] h-5 w-5 fill-current stroke-skyblue text-skyblue'
                   >
                     <g>
                       <g>
@@ -200,7 +243,7 @@ export default function ProductDetail() {
                   </svg>
                   Thêm vào giỏ hàng
                 </button>
-                <button className='fkex bg-orange hover:bg-orange/90 ml-4 h-12 min-w-[5rem] items-center justify-center rounded-sm px-5 capitalize text-white shadow-sm outline-none'>
+                <button className='fkex ml-4 h-12 min-w-[5rem] items-center justify-center rounded-sm bg-skyblue px-5 capitalize text-white shadow-sm outline-none hover:bg-skyblue/90'>
                   Mua ngay
                 </button>
               </div>
@@ -208,8 +251,8 @@ export default function ProductDetail() {
           </div>
         </div>
       </div>
-      <div className='mt-8 bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
           <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>
             Mô tả sản phẩm
           </div>
