4441ea286b5a0c14d31d56d33a1cfaaa40d8eaf2
thecuong1396 <cuongphan@coinmap.tech>
2023-03-07 14:29:17 +0700

    xử lý khi logout thì remove giỏ hàng và xóa data trong giỏ hàng luôn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,6 +16,7 @@ import purchaseApi from 'src/apis/purchase.api'
 import { purchasesStatus } from 'src/constants/purchase'
 import { formatCurrency } from 'src/utils/utils'
 import noproduct from 'src/assets/no-product.png'
+import { queryClient } from 'src/main'
 
 type FormData = Pick<Schema, 'name'>
 
@@ -34,14 +35,15 @@ export default function Header() {
   // (Tất nhiên là trừ trường hợp logout rồi nhảy sang RegisterLayout rồi nhảy vào lại)
   // Nên các query này sẽ không bị inactive => Không bị gọi lại => không cần thiết phải set stale: Infinity
 
-  const { data: purchasesInCartData } = useQuery({
-    queryKey: ['purchases', { status: purchasesStatus.inCart }],
-    queryFn: () => purchaseApi.getPurchases({ status: purchasesStatus.inCart })
-  })
-
-  const purchasesInCart = purchasesInCartData?.data.data
   const { isAuthenticated, setIsAuthenticated, setProfile, profile } =
     useContext(AppContext)
+
+  const { data: purchasesInCartData } = useQuery({
+    queryKey: ['purchases', { status: purchasesStatus.inCart }],
+    queryFn: () => purchaseApi.getPurchases({ status: purchasesStatus.inCart }),
+    enabled: isAuthenticated // khi đã logout ra thì sẽ không gọi api cảu cart nữa
+  })
+  const purchasesInCart = purchasesInCartData?.data.data
   const navigate = useNavigate()
   const logoutMutation = useMutation({
     mutationFn: authApi.logout,
@@ -50,6 +52,10 @@ export default function Header() {
       setIsAuthenticated(false)
       navigate('/')
       setProfile(null)
+      queryClient.removeQueries({
+        // khi logout thì xóa data đang còn lưu trữ trong cart luôn
+        queryKey: ['purchases', { status: purchasesStatus.inCart }]
+      })
     }
   })
 
@@ -290,9 +296,11 @@ export default function Header() {
                     d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
                   />
                 </svg>
-                <span className='absolute top-[-5px] left-[17px] rounded-full bg-white px-[9px] py-[1px] text-xs text-skyblue '>
-                  {purchasesInCart?.length}
-                </span>
+                {purchasesInCart && (
+                  <span className='absolute top-[-5px] left-[17px] rounded-full bg-white px-[9px] py-[1px] text-xs text-skyblue '>
+                    {purchasesInCart?.length}
+                  </span>
+                )}
               </Link>
             </Popover>
           </div>
