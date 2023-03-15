1063a1c24e11b245eeb9ea8ec2932b4eb6e20ac3
thecuong1396 <cuongphan@coinmap.tech>
2023-03-15 22:22:27 +0700

    sử dụng lazy và Suspense của react

--- src/useRouterElements.tsx
+++ src/useRouterElements.tsx
@@ -1,20 +1,34 @@
-import React, { useContext } from 'react'
+import React, { Suspense, lazy, useContext } from 'react'
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
-import Register from './pages/Register'
-import ProductList from './pages/ProductList'
-import Login from './pages/Login'
+// import Register from './pages/Register'
+// import ProductList from './pages/ProductList'
+// import Login from './pages/Login'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
 import { AppContext } from './context/App.context'
 import path from './constants/path'
-import ProductDetail from './pages/ProductDetail'
-import Cart from './pages/Cart'
+// import ProductDetail from './pages/ProductDetail'
+// import Cart from './pages/Cart'
 import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/Components/UserLayout'
-import ChangePassword from './pages/User/pages/ChangePassword'
-import Profile from './pages/User/pages/Profile'
-import HistoryPurchase from './pages/User/pages/HistoryPurchase'
-import NotFound from './pages/NotFound'
+// import ChangePassword from './pages/User/pages/ChangePassword'
+// import Profile from './pages/User/pages/Profile'
+// import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+// import NotFound from './pages/NotFound'
+
+const Login = lazy(() => import('./pages/Login'))
+const ProductList = lazy(() => import('./pages/ProductList'))
+const Profile = lazy(() => import('./pages/User/pages/Profile'))
+const Register = lazy(() => import('./pages/Register'))
+const ProductDetail = lazy(() => import('./pages/ProductDetail'))
+const Cart = lazy(() => import('./pages/Cart'))
+const ChangePassword = lazy(() => import('./pages/User/pages/ChangePassword'))
+const HistoryPurchase = lazy(() => import('./pages/User/pages/HistoryPurchase'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
+// lazy của react, để xử lý việc dùng component đến đâu thì nó mới tải js đến đó, chứ không tải trước, nên cân nhắc mỗi khi dùng
+// Suspense của react là để bao bọc component có sử dụng lazy lại, trong Suspense có attribute là fallback dùng để show ra cái gì đó khi component chưa tải xong
+
 export default function useRouterElements() {
   const ProtectedRoute = () => {
     const { isAuthenticated } = useContext(AppContext)
@@ -24,28 +38,48 @@ export default function useRouterElements() {
     const { isAuthenticated } = useContext(AppContext)
     return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
   }
+
   const routesElement = useRoutes([
     {
       path: '',
-      index: true,
-      element: (
-        <MainLayout>
-          <ProductList />
-        </MainLayout>
-      )
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: path.login,
+          element: (
+            <RegisterLayout>
+              <Suspense fallback>
+                <Login />
+              </Suspense>
+            </RegisterLayout>
+          )
+        },
+        {
+          path: path.register,
+          element: (
+            <RegisterLayout>
+              <Suspense>
+                <Register />
+              </Suspense>
+            </RegisterLayout>
+          )
+        }
+      ]
     },
     {
       path: '',
       element: <ProtectedRoute />,
       children: [
-        // {
-        //   path: path.profile,
-        //   element: (
-        //     <MainLayout>
-        //       <Profile />
-        //     </MainLayout>
-        //   )
-        // },
+        {
+          path: path.cart,
+          element: (
+            <CartLayout>
+              <Suspense>
+                <Cart />
+              </Suspense>
+            </CartLayout>
+          )
+        },
         {
           path: path.user,
           element: (
@@ -56,64 +90,60 @@ export default function useRouterElements() {
           children: [
             {
               path: path.profile,
-              element: <Profile />
+              element: (
+                <Suspense>
+                  <Profile />
+                </Suspense>
+              )
             },
             {
               path: path.changePassword,
-              element: <ChangePassword />
+              element: (
+                <Suspense>
+                  <ChangePassword />
+                </Suspense>
+              )
             },
             {
               path: path.historyPurchase,
-              element: <HistoryPurchase />
+              element: (
+                <Suspense>
+                  <HistoryPurchase />
+                </Suspense>
+              )
             }
           ]
-        },
-        {
-          path: path.cart,
-          element: (
-            <CartLayout>
-              <Cart />
-            </CartLayout>
-          )
         }
       ]
     },
     {
       path: path.productDetail,
-      index: true,
       element: (
         <MainLayout>
-          <ProductDetail />
+          <Suspense>
+            <ProductDetail />
+          </Suspense>
         </MainLayout>
       )
     },
     {
       path: '',
-      element: <RejectedRoute />,
-      children: [
-        {
-          path: path.register,
-          element: (
-            <RegisterLayout>
-              <Register />
-            </RegisterLayout>
-          )
-        },
-        {
-          path: path.login,
-          element: (
-            <RegisterLayout>
-              <Login />
-            </RegisterLayout>
-          )
-        }
-      ]
+      index: true,
+      element: (
+        <MainLayout>
+          <Suspense>
+            <ProductList />
+          </Suspense>
+        </MainLayout>
+      )
     },
     {
       path: '*',
       element: (
         <MainLayout>
-          <NotFound />
+          <Suspense>
+            <NotFound />
+          </Suspense>
         </MainLayout>
       )
     }
