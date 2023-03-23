853fe0fe0f8ab6581b9d28d77951f7f2fc6dec7c
thecuong1396 <cuongphan@coinmap.tech>
2023-03-24 01:31:36 +0700

    Test lỗi nhập data không đúng định dạng trên form

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,23 +1,52 @@
-import { screen, waitFor } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 import path from 'src/constants/path'
 import { logScreen, renderWithRouter } from 'src/utils/testutils'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, beforeAll } from 'vitest'
 import matchers from '@testing-library/jest-dom/matchers'
 expect.extend(matchers)
 
 describe('Login', () => {
-  it('Hiển thị lỗi required khi không nhập gì', async () => {
-    const { user } = renderWithRouter({ route: path.login }) //render được tới trang login
+  beforeAll(async () => {
+    renderWithRouter({ route: path.login }) //render được tới trang login
     await waitFor(() => {
       //sau đó kiểm tra xem có đoạn PlaceholderText là Email hay ko, nếu có thì nghĩa là đã đúng và đúng thì chạy xuống dưới
       expect(screen.queryByPlaceholderText('Email')).toBeInTheDocument()
     })
+  })
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
     const submitButton = document.querySelector(
       'form button[type="submit"]'
     ) as Element
-    user.click(submitButton) //sau đó nhấn submit và hiện tại trong input chứa nhập liệu gì
-    expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
-    expect(await screen.findByText('Password là bắt buộc')).toBeTruthy() // nếu nó show ra 2 đoan test này thì nghĩa là nó đã có hành động validate và show ra 2 dòng thông báo trên và nó show ra thì ta tìm thấy nó nghĩa là đúng vì cần nhập dữ liệu vào thì mới không báo lỗi khi nhấn submit
+    fireEvent.submit(submitButton)
+    await waitFor(async () => {
+      expect(await screen.findByText('Email là bắt buộc')).toBeTruthy()
+      expect(await screen.findByText('Password là bắt buộc')).toBeTruthy() // nếu nó show ra 2 đoan test này thì nghĩa là nó đã có hành động validate và show ra 2 dòng thông báo trên và nó show ra thì ta tìm thấy nó nghĩa là đúng vì cần nhập dữ liệu vào thì mới không báo lỗi khi nhấn submit
+    })
+  })
+  it('Hiển thị lỗi required khi không nhập gì', async () => {
+    const emailInput = document.querySelector(
+      'form input[type="email"]'
+    ) as HTMLInputElement
+    const passwordInput = document.querySelector(
+      'form input[type="password"]'
+    ) as HTMLInputElement
+    const submitButton = document.querySelector(
+      'form button[type="submit"]'
+    ) as HTMLButtonElement
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'test@mail'
+      }
+    })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123'
+      }
+    })
+    fireEvent.submit(submitButton)
+    expect(await screen.findByText('Email không đúng định dạng')).toBeTruthy()
+    expect(await screen.findByText('Độ dài từ 6 - 160 ký tự')).toBeTruthy()
+
     // await logScreen()
   })
 })
